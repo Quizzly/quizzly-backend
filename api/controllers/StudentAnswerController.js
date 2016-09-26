30a5c3484c7fdb1e44008f45df25ed4679ae0a3b
Kevin Chen <chen884@usc.edu>
2016-09-26 12:53:14 -0700

    metrics route for studentanswers

--- api/controllers/StudentAnswerController.js
+++ api/controllers/StudentAnswerController.js
@@ -7,6 +7,40 @@
 
 module.exports = {
 
+	getMetrics: function(req, res){
+		var questions = Question.find({
+        "quiz": req.param('quiz')
+    })
+		.populate('answers')
+    .then(function (questions){
+			var studentanswers = req.param('studentanswers');
+			//Assumes that questions and studentanswer questions are in the same order
+			for(var i = 0; i < studentanswers.length; i++){
+				if(questions[i].answers.length > 0){
+					for(var j = 0; j < questions[i].answers.length; j++){
+						//Assumes that students answer all multiple choice questions
+						console.log(studentanswers[i].answer.id);
+						console.log(questions[i].answers[j].id);
+						console.log(studentanswers[i].answer.correct);
+						if((studentanswers[i].answer.id == questions[i].answers[j].id) && (studentanswers[i].answer.correct == 'false')){
+							console.log("here");
+							questions[i].answers[j].studentSelectedIncorrect = true;
+							break;
+						}
+					}
+
+				}
+				else{
+					if(studentanswers[i].text){
+						questions[i].answerText = studentanswers[i].text;
+					}
+				}
+
+			}
+			console.log(questions);
+      return res.json(questions);
+    });
+	},
 
 	getStudentCountByAnswerId: function(req,res) {
     	console.log("--------------getStudentCountByAnswerId");
@@ -19,18 +53,17 @@ module.exports = {
             StudentAnswer.count({answer: id}).exec(function(err, found){
             console.log("found: ", found);
             return res.json(found);
-            
+
         });
         } else {
         // console.log("data.section", data.section);
     	StudentAnswer.count({answer: id, section: section}).exec(function(err, found){
     		console.log("found: ", found);
     		return res.json(found);
-    		
+
     	});
     }
 	}
 
-	
-};
 
+};
