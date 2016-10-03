867d376d45840f6b5083570014fc17494a6809d5
Benny Lau <bennycwlau@gmail.com>
2016-10-02 20:55:57 -0700

    added number of correct/incorrect answers per quiz for a given student functionality

--- api/controllers/SectionController.js
+++ api/controllers/SectionController.js
@@ -39,39 +39,81 @@ module.exports = {
     })
   },
 
-  getStatisticsForSectionQuiz: function(req, res) {
+  numberOfCorrectAnswersPerStudent: function(req, res) {
     var data = req.params.all();
     var sectionId = data.sectionId;
     var quizId = data.quizId;
     var correctStudentAnswers = {};
 
     StudentAnswer.find({section: sectionId, quiz: quizId})
-    .populate('student')
+    .populate('student') //Take the ObjectId of the linked student model and replace it with the object corresponding to that ObjectId
+    .populate('answer') ////Take the ObjectId of the linked answer model and replace it with the object corresponding to that ObjectId
+    .then(function(studentAnswers) {
+      for(var i = 0; i < studentAnswers.length; i++) { //For each student answer corresponding to the quiz and section
+        var studentAnswer = studentAnswers[i];
+        if(correctStudentAnswers[studentAnswer.student.id] == undefined) { //If student is not in map
+          correctStudentAnswers[studentAnswer.student.id] = { //Key: Student ID
+            correct: 0, //Value: (Correct number of answers, First name)
+            name: studentAnswer.student.firstName
+          };
+        }
+        else { //Student already in map
+          if(studentAnswer.answer.correct) {  //If the student's answer is correct
+            correctStudentAnswers[studentAnswer.student.id].correct++; //Increment the count of the number of correct answers it has in the value pair
+          }
+        }
+      }
+      var correctAnswers = []; //New array to put the value pairs in (exclude keys from map)
+      for(var studentId in correctStudentAnswers) { //For each key or student in the map
+        if(correctStudentAnswers.hasOwnProperty(studentId)) {
+          var correctAnswer = {name: correctStudentAnswers[studentId].name, correct:correctStudentAnswers[studentId].correct}; //Make a pair corresponding to the value pair in the map
+          correctAnswers.push(correctAnswer); //Put it in the array
+        }
+      }
+      res.json(correctAnswers); //Send the array back
+    });
+  },
+
+  numberOfCorrectAndIncorrectAnswersForStudent: function(req, res) {
+    var data = req.params.all();
+    var studentId = data.studentId;
+    var sectionId = data.sectionId;
+    var numberOfCorrectAndIncorrectAnswers = {};
+    var arrayNumberOfCorrectAndIncorrectAnswers = []; //New array to put the value pairs in (exclude keys from map)
+    StudentAnswer.find({section: sectionId, student:studentId})
+    .populate('quiz')
+    .populate('question')
     .populate('answer')
     .then(function(studentAnswers) {
       for(var i = 0; i < studentAnswers.length; i++) {
         var studentAnswer = studentAnswers[i];
-        if(correctStudentAnswers[studentAnswer.student.id] == undefined) {
-          correctStudentAnswers[studentAnswer.student.id] = {
+        if(numberOfCorrectAndIncorrectAnswers[studentAnswer.quiz.title] == undefined) {
+          numberOfCorrectAndIncorrectAnswers[studentAnswer.quiz.title] = {
             correct: 0,
-            name: studentAnswer.student.firstName
+            incorrect: 0
           };
+        }
+        if(studentAnswer.question.type == "freeResponse") {
+          numberOfCorrectAndIncorrectAnswers[studentAnswer.quiz.title].correct++;
         }
         else {
           if(studentAnswer.answer.correct) {
-            correctStudentAnswers[studentAnswer.student.id].correct++;
+            numberOfCorrectAndIncorrectAnswers[studentAnswer.quiz.title].correct++;
+          }
+          else {
+            numberOfCorrectAndIncorrectAnswers[studentAnswer.quiz.title].incorrect++;
           }
         }
       }
-      var correctAnswers = [];
-      for(var studentId in correctStudentAnswers) {
-        if(correctStudentAnswers.hasOwnProperty(studentId)) {
-          var correctAnswer = {name: correctStudentAnswers[studentId].name, correct:correctStudentAnswers[studentId].correct};
-          correctAnswers.push(correctAnswer);
+    }).then(function() {
+      for(var quiz in numberOfCorrectAndIncorrectAnswers) { //For each key or student in the map
+        if(numberOfCorrectAndIncorrectAnswers.hasOwnProperty(quiz)) {
+          var entry = {name: quiz, correct:numberOfCorrectAndIncorrectAnswers[quiz].correct, incorrect:numberOfCorrectAndIncorrectAnswers[quiz].incorrect}; //Make a pair corresponding to the value pair in the map
+          arrayNumberOfCorrectAndIncorrectAnswers.push(entry); //Put it in the array
         }
       }
-      res.json(correctAnswers);
+    }).done(function() {
+      res.send(arrayNumberOfCorrectAndIncorrectAnswers); //Put this in done function so it doesn't prematurely send back an empty object
     });
   }
-
 };
