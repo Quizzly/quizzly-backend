bf2ad2783a148f76ec1cb6065cc53353eb549ac0
Daniel Arellano <danny@Daniels-MacBook-Pro.local>
2016-10-05 20:25:22 -0700

    Answer quiz route

--- config/env/development.js
+++ config/env/development.js
@@ -22,22 +22,22 @@ module.exports = {
   // }
 
   pushSettings: {
-    // Android
-    gcm: {
-      id: null, // PUT YOUR GCM SERVER API KEY,
-      msgcnt: 1,
-      dataDefaults: {
-        delayWhileIdle: false,
-        timeToLive: 4 * 7 * 24 * 3600, // 4 weeks
-        retries: 4,
-      },
-      // Custom GCM request options https://github.com/ToothlessGear/node-gcm#custom-gcm-request-options
-      options: {},
-    },
+    // // Android
+    // gcm: {
+    //   id: null, // PUT YOUR GCM SERVER API KEY,
+    //   msgcnt: 1,
+    //   dataDefaults: {
+    //     delayWhileIdle: false,
+    //     timeToLive: 4 * 7 * 24 * 3600, // 4 weeks
+    //     retries: 4,
+    //   },
+    //   // Custom GCM request options https://github.com/ToothlessGear/node-gcm#custom-gcm-request-options
+    //   options: {},
+    // },
 
     // Apple
     apn: {
-      gateway: 'gateway.sandbox.push.apple.com',
+      gateway: 'com.quizzly.mobile',
       badge: 1,
       defaultData: {
         expiry: 4 * 7 * 24 * 3600, // 4 weeks
