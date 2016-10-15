c5d6da89adfdd8a32008c2212985f69d400d88a0
Daniel Arellano <danny@Daniels-MacBook-Pro.local>
2016-10-15 16:29:15 -0700

    Uncommented push code for now

--- api/services/Push.js
+++ api/services/Push.js
@@ -8,20 +8,20 @@
 
 var apn = require('apn');
 var settings = sails.config.pushSettings;
-var apnProvider = new apn.Provider(settings.apn.options);
+// var apnProvider = new apn.Provider(settings.apn.options);
 
 module.exports = {
   send: function(deviceIds, data, callback) {
-    var note = {};
-    note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-    note.badge = settings.apn.data.badge;
-    note.sound = settings.apn.data.sound;
-    note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
-    note.payload = {'messageFrom': 'Quizzly'};
-    note.topic = settings.apn.gateway;
-
-    apnProvider.send(note, deviceIds).then( (result) => {
-      callback(result);
-    });
+    // var note = {};
+    // note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+    // note.badge = settings.apn.data.badge;
+    // note.sound = settings.apn.data.sound;
+    // note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+    // note.payload = {'messageFrom': 'Quizzly'};
+    // note.topic = settings.apn.gateway;
+    //
+    // apnProvider.send(note, deviceIds).then( (result) => {
+    //   callback(result);
+    // });
   }
 };
